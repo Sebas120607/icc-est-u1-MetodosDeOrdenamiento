400597d44bd05ac6291d19128708b3dc9810ea89
Sebas Lima <cabreralimasebastian123@gmail.com>
2026-04-22 08:57:46 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -1,26 +1,34 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int[] numeros = {10, 0, -5, 5, 15, 2};
-        runBubbleSort(); 
+public class App {  
+    
+    public static void main(String[] args) throws Exception{
+        int[] numeros ={ 10, 0, -5, 5, 15, 2};
+        //runBubbleSort();
+        runBubbleSortAvz();
     }
-    public static void runBuddlesort(){
-        System.out.println("Metodo Burbuja");
-        int[] numeros = {-5, 10, 2, 0, 7};
-        //instanciar de la clase 
-        BubbleSort bubbleSort = new BubbleSort();
-        //bubbleSort.printArreglo(numeros);
-        //bubbleSort.sortAscendente (numeros);
-        //bubbleSort.printArreglo (numeros);
-        //bubbleSort.sortDescendet(numeros);
-        //bubbleSort.printArreglo (numeros);
 
-        bubbleSort.printArreglo (numeros);
-        bubbleSort.sort(numeros, asc: true);
-        bubbleSort.printArreglo (numeros);
-        bubbleSort.sort(numeros, asc: false);
-         bubbleSort.printArreglo (numeros);
-
-
-
+    public static void runBubbleSort(){
+        // System.out.println("metodo burbuja");
+        // int[]numeros = {-5, 10, 2, 0, 7};
+        // //instanciar una clase
+        // BubbleSort bubbleSort= new BubbleSort();
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sortAscendete(numeros);
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sort(numeros,true);
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sort(numeros, false);
+        // bubbleSort.sort(numeros);
     }
+
+    public static void runBubbleSortAvz(){
+        int [] array = new int[]{9,2,3,0,8,5};
+
+        //instancia la clase
+        BubbleSortAvz bSortAvz =new BubbleSortAvz(array);
+        bSortAvz.printArray();
+        bSortAvz.sort(true);
+        bSortAvz.printArray();
+    }
+    
 }
--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,25 +1,5 @@
-public class BubbleSort {
-    public BubbleSort(){
-        System.out.println("Se creo la clase en el constructor ");
-    }
-
-    
-    public void sortAscendente (int numeros[]) {
-        for (int i = 0 ;  i <numeros.length; i++){
-            for (int j = i + 1; j < numeros.length; j++){
-                //comparacion
-                if (numeros[i]> numeros[j]){
-                // si cumple -> intercambio
-                    int aux = numeros[1];
-                    numeros[i]= numeros [j];
-                    numeros[j]= aux; 
-                }
-
-            }
-        }
-    }
-
-    public void sortDescendet (int[] numeros) {
+   public class BubbleSort{
+   public static void sortDescendet (int[] numeros) {
          for (int i = 0 ;  i >numeros.length; i++){
             for (int j = i + 1; j < numeros.length; j++){
                 //comparacion
@@ -47,14 +27,14 @@ public class BubbleSort {
         /// 
         public void sort(int[] numeros, boolean asc){
             if(asc){
-                sortAscendente(numeros);
+                sortAscendent(numeros);
             }else{
                 sortDescendet(numeros);
             }
         }
+ }   
 
-
-        }
+        
        
            
         
